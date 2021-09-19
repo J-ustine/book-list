c90d8f65390da3505b16c9771224ae54ba29a536
J-ustine <justine.collache@gmail.com>
2021-09-19 16:36:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Getting Started with Book List App
+
+This project is for you to store a book list that you have read or you want to read.
+The objective is to have comment, rating and much more features.
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Welcome to your book list app!</h3>
+        <p>
+          Soon you will be able to track your favorite books, adding comments,
+          rewiews and rate!
+        </p>
+      </header>
+    </div>
+  );
+}
