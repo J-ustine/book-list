7877bad945d8e19dadf7b5ffa28582a5870e9b9c
J-ustine <justine.collache@gmail.com>
2021-09-21 17:50:07 +0200

    Add soon message in each route

--- src/Suggestions.js
+++ src/Suggestions.js
@@ -30,6 +30,15 @@ export default function Suggestions() {
 import React from "react";
 
 export default function Suggestions() {
+<<<<<<< HEAD
   return "Suggestions";
 >>>>>>> 4b37e6d9... Add React router and routes to the app
+=======
+  return (
+    <header className="App-header">
+      <h3>Welcome to your list of suggestion book!</h3>
+      <p>Soon, you will have a personalized suggestion list.</p>
+    </header>
+  );
+>>>>>>> 42355642... Add soon message in each route
 }
