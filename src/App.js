3d0961430823f94ae6e92756729804858483e29f
J-ustine <justine.collache@gmail.com>
2021-09-21 15:29:21 +0200

    Solve rebase conflict

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
 import "./App.css";
-import SearchBar from "./SearchBar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import MyList from "./MyList";
+import Suggestions from "./Suggestions";
+import Home from "./Home";
 
 export default function App() {
   return (
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            Booklist
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  My list
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Sugestions
-                </a>
-              </li>
-            </ul>
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <Link to="/">Booklist</Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/my-list">My List</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/suggestions">Suggestions</Link>
+                </li>
+              </ul>
+            </div>
           </div>
+        </nav>
+        <div className="content">
+          <Switch>
+            <Route exact path="/my-list">
+              <MyList />
+            </Route>
+            <Route exact path="/suggestions">
+              <Suggestions />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
         </div>
-      </nav>
-      <header className="App-header">
-        <h3>Welcome to your book list app!</h3>
-        <p>
-          Soon you will be able to track your favorite books, adding comments,
-          rewiews and rate!
-        </p>
-      </header>
-      <SearchBar />
-    </div>
+      </div>
+    </Router>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <header className="App-header">
+      <h3>Welcome to your book list app!</h3>
+      <p>
+        Soon you will be able to track your favorite books, adding comments,
+        rewiews and rate!
+      </p>
+    </header>
+  );
+}
