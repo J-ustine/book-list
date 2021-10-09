2448132237b3986e480ba84b676ff76a71a86073
J-ustine <justine.collache@gmail.com>
2021-10-09 11:38:11 +0200

    Render data from MongoDB in the suggestions component

--- client/src/Suggestions.js
+++ client/src/Suggestions.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import SearchResult from "./SearchResult";
 
 export default function Suggestions() {
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("/suggestion");
+      setSuggestions(req.data);
+    }
+    fetchData();
+  }, []);
+
   return (
     <header className="App-header">
-      <h3>Welcome to your list of suggestion book!</h3>
-      <p>Soon, you will have a personalized suggestion list.</p>
+      <h3>Welcome to your list of suggested book!</h3>
+      <SearchResult result={suggestions} />
     </header>
   );
 }
