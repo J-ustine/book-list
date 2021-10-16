79075482b82705c918daa310efc5d7a3fdebc5d7
J-ustine <justine.collache@gmail.com>
2021-10-16 15:42:00 +0200

    Add selected book to myList component

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -1,44 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function SearchResult(props) {
-  const [title, setTitle] = useState(null);
-  const [author, setAuthor] = useState(null);
-  const [comments, setComments] = useState(null);
-  const [rating, setRating] = useState(null);
+  const [book, setBook] = useState({
+    author: null,
+    title: null,
+    comments: null,
+    rating: null,
+  });
 
   async function test2(event) {
+    console.log(event.target.name);
     event.preventDefault();
-    // await addSuggestion(event).then(() => {
-    try {
-      const response = fetch("/add", {
-        method: "post",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          author: "Justine",
-          title: "Hello from title",
-          comments: "comments",
-          rating: 5,
-        }),
-      });
-      const body = (await response).json();
-      console.log("Completed!", body);
-    } catch (err) {
-      console.error(`Error: ${err}`);
-    }
-    // });
-  }
-
-  function addSuggestion(event) {
-    return new Promise(() => {
-      event.preventDefault();
-      setTitle(event.target.title);
-      setAuthor(event.target.author);
-      // setComments(prompt("Please add a comment to this book"));
-      // setRating(
-      //   prompt("Please rate this book from 1(disapointed) to 5(love it)")
-      // );
+    setBook({
+      author: event.target.name,
+      title: event.target.title,
+      comments: prompt("Please let a review for this book"),
+      rating: prompt("Please rate this book from 1(disapointed) to 5(love it)"),
     });
   }
+
+  useEffect(() => {
+    async function post() {
+      if (book.title !== null) {
+        try {
+          const response = fetch("/add", {
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              author: `${book.author}`,
+              title: `${book.title}`,
+              comments: `${book.comments}`,
+              rating: `${book.rating}`,
+            }),
+          });
+          const body = (await response).json();
+          console.log("Completed!", body);
+        } catch (err) {
+          console.error(`Error: ${err}`);
+        }
+      }
+    }
+    post();
+  }, [book]);
 
   return (
     <div className="row search-result">
@@ -55,7 +58,8 @@ export default function SearchResult(props) {
                   <button
                     className="btn btn-success"
                     onClick={test2}
-                    name="add"
+                    name={result.author}
+                    title={result.title}
                   >
                     Add to my list
                   </button>
